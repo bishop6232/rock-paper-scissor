56972bcbeb50a975d7d0a776731f370789f21fbb
bishop6232 <104029083+bishop6232@users.noreply.github.com>
2023-06-17 13:52:19 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,83 @@
+function rock(){
+
+const randomNumber = Math.random();
+let computerMove;
+let result ;
+
+if(randomNumber >= 0 && randomNumber < 1/2){
+    computerMove = 'rock';
+}
+else if(randomNumber >= 1/2 && randomNumber < 2/3){
+    computerMove = 'paper';
+}
+else if(randomNumber >= 2/3 && randomNumber < 1){
+    computerMove = 'scissors';
+}
+
+
+if(computerMove === 'rock'){
+    result = 'tie!';
+}
+else if(computerMove ==='paper'){
+    result = 'you lose!';
+}
+else if(computerMove === 'scissors'){
+    result = 'you win!';
+}
+alert(`you selected rock, computer selected ${computerMove}, ${result}`)
+}
+
+function paper(){
+    
+    const randomNumber = Math.random();
+    
+    if(randomNumber >= 0 && randomNumber < 1/3){
+        computerMove = 'rock';
+    }
+    else if(randomNumber >= 1/3 && randomNumber < 2/3){
+        computerMove = 'paper';
+    }
+    else if(randomNumber >= 2/3 && randomNumber < 1){
+        computerMove = 'scissors';
+    }
+    
+    let result;
+    if(computerMove === 'rock'){
+        result = 'you win!'
+    }
+    if(computerMove === 'paper'){
+        result = 'tie!'
+    }
+    if(computerMove === 'scissors'){
+        result = 'you lose!'
+    }
+    alert(`you selcted paper, computer selected ${computerMove}, ${result}`);
+}
+
+function scissors(){
+    
+    const randomNumber = Math.random();
+    
+    if( randomNumber >= 0 && randomNumber < 1/3){
+        computerMove = 'rock'
+    }
+    else if(randomNumber >= 1/3 && randomNumber < 2/3){
+        computerMove = 'paper';
+    }
+    else if(randomNumber >= 2/3 && randomNumber < 1){
+        computerMove = 'scissors';
+    }
+
+
+    let result;
+    if(computerMove === 'rock'){
+        result = 'you lose!';
+    }
+    else if(computerMove === 'paper'){
+        result = 'you win!';
+    }
+    else if(computerMove === 'scissors'){
+        result = 'tie!';
+    }
+    alert(`you selected scissors, computer selected ${computerMove}, ${result}`);
+}
